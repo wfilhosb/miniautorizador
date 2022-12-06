a734f4bbca6bb521bf9eaabda161a88a4df41ab9
wfilhosb <wfilhosb@hotmail.com>
2022-12-06 18:59:05 -0300

    chore: application.java initial settings

--- mini-autorizador/src/main/java/com/miniautorizador/MiniAutorizadorApplication.java
+++ mini-autorizador/src/main/java/com/miniautorizador/MiniAutorizadorApplication.java
@@ -1,9 +1,23 @@
 package com.miniautorizador;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"com.miniautorizador.model"})
+@ServletComponentScan(basePackages = {"com.miniautorizador.*"})
+@EnableJpaRepositories(basePackages = {"com.miniautorizador.repository"})
+@EnableTransactionManagement
+@EnableWebMvc
+@RestController
+@EnableAutoConfiguration
 public class MiniAutorizadorApplication {
 
 	public static void main(String[] args) {
