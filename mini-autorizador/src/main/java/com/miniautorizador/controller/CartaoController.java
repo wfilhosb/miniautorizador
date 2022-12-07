c69c12405ca0e491671dc0cf990b6c4df8faba5d
wfilhosb <wfilhosb@hotmail.com>
2022-12-07 11:02:52 -0300

    feat: creation of the perform transaction functionality

--- mini-autorizador/src/main/java/com/miniautorizador/controller/CartaoController.java
+++ mini-autorizador/src/main/java/com/miniautorizador/controller/CartaoController.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,14 +29,22 @@ public class CartaoController {
 	public ResponseEntity<Cartao> criarNovo(@RequestBody Cartao cartao){
 		cartao.setSaldoCartao(500);
 		Cartao cartaoCriado = cartaoRepository.save(cartao);
-		return new ResponseEntity<Cartao>(cartaoCriado,HttpStatus.OK);
+		return new ResponseEntity<Cartao>(cartaoCriado,HttpStatusCode.valueOf(201));
 	}
 	
 	//RETORNA SALDO
 	@GetMapping(value = "/{numeroCartao}")
 	public ResponseEntity<Cartao> saldoCartao(@PathVariable(value = "numeroCartao") Long numeroCartao){
 		Optional<Cartao> cartao = cartaoRepository.findById(numeroCartao);
+		//return cartao.get().getSaldoCartao();
 		return new ResponseEntity<Cartao>(cartao.get(), HttpStatus.OK);
+	}
+	
+	//REALIZA UMA TRANSAÇÃO
+	@PostMapping(value = "/transacoes")
+	public ResponseEntity<Cartao> realizaTransacao(@RequestBody Cartao cartao){
+		Cartao cartaoDaTransacao = cartaoRepository.save(cartao);
+		return new ResponseEntity<Cartao>(cartaoDaTransacao,HttpStatusCode.valueOf(201));
 	}
 	
 }
